baec6ff166953362c669e6dca245764ebff57103
hirokaissh <hirokai.saito1014@gmail.com>
2022-03-19 23:43:51 +0900

    keyを定数で定義

--- HIROAKIKadai12/ViewController.swift
+++ HIROAKIKadai12/ViewController.swift
@@ -12,12 +12,14 @@ class ViewController: UIViewController {
     @IBOutlet private weak var taxTextFieild: UITextField!
     @IBOutlet private weak var includingTaxLabel: UILabel!
 
+    private let key = "TaxPrice"
+
     override func viewDidLoad() {
         super.viewDidLoad()
         beforTaxTextField.keyboardType = .numberPad
         taxTextFieild.keyboardType = .decimalPad
 
-        includingTaxLabel.text = UserDefaults.standard.string(forKey: "TaxPrice") ?? ""
+        includingTaxLabel.text = UserDefaults.standard.string(forKey: key) ?? ""
     }
 
     @IBAction private func totalcalc(_ sender: Any) {
@@ -28,6 +30,6 @@ class ViewController: UIViewController {
         totalPrice = Int(priceBeforTax * (1 + (tax / 100.0)))
         includingTaxLabel.text = String(totalPrice)
 
-        UserDefaults.standard.set(String(totalPrice), forKey: "TaxPrice")
+        UserDefaults.standard.set(String(totalPrice), forKey: key)
     }
 }
