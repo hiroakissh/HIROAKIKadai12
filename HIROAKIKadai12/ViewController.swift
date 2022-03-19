6e4d023e4341dfbf1cafb5ad1412e5397786123f
hirokaissh <hirokai.saito1014@gmail.com>
2022-03-19 23:07:43 +0900

    [add] UI部品とViewControllerのアウトレット接続

--- HIROAKIKadai12/ViewController.swift
+++ HIROAKIKadai12/ViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet private weak var beforTaxTextField: UITextField!
+    @IBOutlet private weak var taxTextFieild: UITextField!
+    @IBOutlet private weak var includingTaxTextFileld: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
